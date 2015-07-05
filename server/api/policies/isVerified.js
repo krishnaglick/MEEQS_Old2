6d3d6c15a81691676de848ab50baa6c33e1f89c7
Krishna Glick <unstoppable411@gmail.com>
2015-07-05 17:37:14 -0400

    Added policy for verification.

---
+++ server/api/policies/isVerified.js
@@ -0,0 +1,24 @@
+/**
+ * isVerified
+ *
+ * @module      :: isVerified
+ * @description :: Policy to check if the requesting user is verified
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+
+module.exports = ( req, res, next ) => {
+    Users.findOne({ username: req.user.username }).exec((err, user) => {
+        if(err) {
+            res.status(400);
+            res.send(err);
+        }
+        if(user && !user.isVerified) {
+            res.status(401);
+            res.send('You\'re not verified!');
+        }
+        if(user && user.isVerified) {
+            return next();
+        }
+    });
+};
