454eb9c42c71aca7fd0c6f1a6269c4283d1e49de
Krishna Glick <unstoppable411@gmail.com>
2015-08-24 22:19:45 -0400

    Saying screw it to deep populate, just gonna wait on waterline.

--- server/api/controllers/RestaurantLocationsController.js
+++ server/api/controllers/RestaurantLocationsController.js
@@ -6,33 +6,5 @@
  */
 
 module.exports = {
-  find : (req, res) => {
-    var associations = [
-      ['ratings', 'users'],
-      ['tags']
-    ];
-
-    /*RestaurantLocations.find()
-    .populate('tags')
-    .exec((err, vals) => {
-      return res.ok({restaurantLocations: vals});
-    });*/
-
-    if(true) {
-      Utils.deepPopulate('restaurantLocations', associations)
-      .then((data) => {
-        console.log('--------------------------------');
-        return res.ok({restaurantLocations: data});
-      });
-    }
-    
-    /*RestaurantLocations.find()
-    .populate('ratings')
-    .populate('tags')
-    .exec((err, results) => {
-      if(err) return res.serverError(err);
-
-      return res.ok({restaurantLocations: results});
-    });*/
-  }
+  
 };
