90d1378425e7120c7560ccffee69246a0d04d049
Krishna Glick <unstoppable411@gmail.com>
2015-08-24 23:11:22 -0400

    Added roles. Removed the computed IDs.

--- server/api/models/Ratings.js
+++ server/api/models/Ratings.js
@@ -18,9 +18,6 @@ module.exports = {
       unique: true,
       autoIncrement: true
     },
-    id: function() {
-      return this.ratingID;
-    },
     restaurantLocation: {
       model: 'restaurantLocations',
       required: true
--- server/api/models/Users.js
+++ server/api/models/Users.js
@@ -20,9 +20,6 @@ module.exports = {
       unique: true,
       autoIncrement: true
     },
-    id: function() {
-      return this.userID;
-    },
     username: {
       type: 'string',
       unique: true,
@@ -47,6 +44,9 @@ module.exports = {
     isAdmin: {
       type: 'boolean',
       defaultsTo: false
+    },
+    roles: {
+      model: 'roles'
     },
     isDeleted: {
       type: 'boolean',
